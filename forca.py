b04ea6bba22c95254b006677003e24f84c48391d
Marcelo Nogueira <contato@marcelonogueira.com.br>
2019-11-20 01:37:02 +0000

    continuacao

--- forca.py
+++ forca.py
@@ -8,28 +8,36 @@ def jogar():
 
     enforcou = False
     acertou = False
+    erro = 0
 
     print("A palavra secreta contém {} letra(s). {}".format(len(palavra_secreta), letras_acertadas))
 
-    while (not enforcou and not acertou):
+    while not enforcou and not acertou:
         chute = input("Qual a primeira letra ?")
         chute = chute.strip()
 
         index = 0
         for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index]= letra
+            if chute.upper() == letra.upper():
+                letras_acertadas[index] = letra
             index = index + 1
 
         palavra_completa = ""
         for l in letras_acertadas:
-            palavra_completa = palavra_completa + l
+            palavra_completa += l
 
-        if(palavra_completa == palavra_secreta):
+        if palavra_completa == palavra_secreta:
             acertou = True
-            print("Parabens! Voce acertou a palavra secreta {}.", palavra_completa)
+            print("Parabens! Voce acertou a palavra secreta -> {}.".format(palavra_completa))
+        else:
+            erro += 1
 
-        print(letras_acertadas)
+        enforcou = erro == 6
+
+        if enforcou:
+            print("Fim do Jogo!")
+        else:
+            print(letras_acertadas)
 
 if(__name__ == "__main__"):
     jogar()
